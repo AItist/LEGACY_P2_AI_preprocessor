4a2edae547c37b817a819b9417ef3f115eee1729
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-08 23:29:37 +0900

    Update var_test.py

--- var_test.py
+++ var_test.py
@@ -1,3 +1,10 @@
+# import os
+
+# os.environ['MY_ENV_VAR'] = 'my_value'
+
+# print('hello')
+
 import os
 
-os.environ['MY_ENV_VAR'] = 'my_value'
+my_var = os.environ.get('MY_ENV_VAR')
+print(my_var)
