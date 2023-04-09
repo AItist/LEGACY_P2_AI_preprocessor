132214555629a8bbdd8b3090b05cc2b9cc17965a
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-09 23:58:58 +0900

    feat, chore: 0.0.2 이미지 수신 - 가공 - 전달 웹소켓 코드 정리

--- main.py
+++ main.py
@@ -11,8 +11,15 @@ async def receive_stream_data():
                 # await websocket.send("Hello, server!")
                 # print(f">>> Hello, server!")
 
+                # 이미지를 받을때 처리
                 greeting = await websocket.recv()
                 print(f"<<< {greeting}")
+
+                # TODO: 이미지 처리 구간
+
+                # 이미지를 보낼때 처리
+                await websocket.send("client send 이미지 처리 완료, 이미지 전달함")
+                print(f">>> 이미지 처리 완료, 이미지 전달함")
         except KeyboardInterrupt:
             # clean up resources here
             pass
